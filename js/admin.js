2882e2dee6fa4f5f91268b90e03d189bdebf3407
xgl <1549742101@qq.com>
2020-09-23 09:34:02 +0800

    init front-end

---
+++ js/admin.js
@@ -0,0 +1,6 @@
+$(function(){
+    let menu = document.querySelector(".menu-tree");
+    console.log(menu)
+    let menuParent = document.querySelectorAll(".menu>.menu-tree>.parent");
+    console.log(menuParent)
+})
